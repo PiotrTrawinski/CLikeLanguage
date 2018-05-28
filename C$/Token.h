8207ca2a8b9600f7a070d3932db0d7f97868d04c
Piotr Trawinski <trawinski.piotr@poczta.fm>
2018-05-28 13:45:54 +0200

    Made token class cleaner

--- C$/Token.h
+++ C$/Token.h
@@ -10,60 +10,12 @@ struct Token {
         Float,          // starts with digit, has exactly 1 '.' in the middle
         Symbol          // any 1 symbol that does not match anything above
     };
+
+    Token(Type type, std::string value) : 
+        type(type),
+        value(value)
+    {}
     
-    virtual Type type()=0;
-};
-
-struct TokenLabel : Token {
-    TokenLabel(std::string value) : value(value){}
-    Type type() override {
-        return Token::Type::Label;
-    }
-
+    Type type;
     std::string value;
-};
-
-struct TokenStringLiteral : Token {
-    TokenStringLiteral(std::string value) : value(value){}
-    Type type() override {
-        return Token::Type::StringLiteral;
-    }
-
-    std::string value;
-};
-
-struct TokenChar : Token {
-    TokenChar(char value) : value(value){}
-    Type type() override {
-        return Token::Type::Char;
-    }
-
-    char value;
-};
-
-struct TokenInteger : Token {
-    TokenInteger(std::string value) : value(value){}
-    Type type() override {
-        return Token::Type::Integer;
-    }
-
-    std::string value;
-};
-
-struct TokenFloat : Token {
-    TokenFloat(std::string value) : value(value){}
-    Type type() override {
-        return Token::Type::Float;
-    }
-
-    std::string value;
-};
-
-struct TokenSymbol : Token {
-    TokenSymbol(char value) : value(value){}
-    Type type() override {
-        return Token::Type::Symbol;
-    }
-
-    char value;
 };
