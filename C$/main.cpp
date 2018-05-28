226c4f93e9b254570ac0eda43d1de2ff97ae5b55
Piotr Trawinski <trawinski.piotr@poczta.fm>
2018-05-28 14:02:38 +0200

    main is now using parsing function

--- C$/main.cpp
+++ C$/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include "parsing.h"
 
-int main() {
+using namespace std;
+
+int main(char** argv, int argc) {
+    if (argc < 2) {
+        return 1;
+    }
+    auto tokens = parseFile(argv[1]);
+    if (!tokens) {
+        cerr << "Compiling failed: there were errors during parsing\n";
+        return 2;
+    }
+
     return 0;
 }
